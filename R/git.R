15881bb0d3c46431c3c3b8a485f7e264f0f0a7b0
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2019-04-30 11:32:18 +0100

    fixes to git_info

--- R/git.R
+++ R/git.R
@@ -1,22 +1,33 @@
 #' Git information
 #' 
-#' Displays git information from the repository
+#' Displays git information from the repository of the current working space.
 #' 
 #' @import git2r
 #' @import shiny
 #' @export
 git_info <- function() {
-  print(getwd())
-  repo <- init(path = ".", bare = FALSE)
-  branch <- repository_head(repo)[[1]]
-  if (is.null(branch)) {
-    git_message <- "Empty repo."
+  repo <- tryCatch({
+            repository(".")
+          }, error = function(e) {
+             NULL
+          })
+  if (is.null(repo)) {
+    git_message <- "It looks like you're not in a valid git repo."
   } else {
-    last_commit <- as.character(commits(repo)[[1]])[[4]]
-    git_message <- span(paste("Branch:", branch),
-                        br(),
-                        paste("Last commit: ", last_commit)
-                       )
+    branch <- repository_head(repo)[[1]]
+    if (is.null(branch)) {
+      git_message <- "Empty repo."
+    } else {
+      last_commit <- as.character(commits(repo)[[1]]$message)
+      changes <- ""
+      if (length(stat$unstaged$modified) + length(stat$staged$modified) > 0)
+        changes <- "(!) Repo has some not commited changes"
+      git_message <- span(paste("Branch:", branch),
+                          br(),
+                          paste("Last commit: ", last_commit),
+                          br(), changes
+                      )
+    }
   }
   display(git_message)
 }
