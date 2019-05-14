30bbd9c9f20d5f6a12ae95189550b21f5fc4289a
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2019-05-14 15:53:47 +0100

    prototype of powered by solution

--- R/display.R
+++ R/display.R
@@ -5,16 +5,43 @@
 #' @return div which wraps your message to display it in the top-right corner of your shiny app.
 #' @export
 display <- function(message) {
-  style = "
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: auto;
-    height: auto;
-    background-color: #E0E0E0;
-    padding: 3px 8px;
-    font-size: 12px;
-    z-index = 9999;"
+  tagList(
+    tags$head(
+      tags$style(
+        HTML("
+        #shinyinfo {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: auto;
+        height: auto;
+        color: #000000;
+        background-color: #f5f5f5;
+        padding: 3px 8px;
+        font-size: 12px;
+        z-index = 9999;
+        }
+        #shinyinfo a {
+        color: #0099f9;
+        }
+        ")
+      )
+    ),
+    div(id = "shinyinfo", message)
+  )
+}
 
-  div(style = style, message)
+
+#' Powered by
+#' 
+#' Displays information about authors of the shiny app.
+#'
+#' @param company_name character with the creator of the app
+#' @param link link to the creator's website
+#'
+#' @return div with "powered by".
+#' @export
+#' @importFrom shiny a p
+powered_by <- function(company_name, link="#") {
+  display(p("Powered by ", a(href = link, target="_blank", company_name)))
 }
