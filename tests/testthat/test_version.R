fb04b1c16ffce3bc3b68fabb528be488786106f8
Jakub Nowicki <kuba@appsilon.com>
2020-01-31 14:00:35 +0100

    lint

--- tests/testthat/test_version.R
+++ tests/testthat/test_version.R
@@ -8,7 +8,7 @@ test_that("test version", {
   expect_true(any(grepl("<div id=\"shinyinfo_version\" class=\"info_box\">1.0.0</div>", si_str, fixed = TRUE)))
   expect_true(any(grepl("1.0.0", si_str, fixed = TRUE)))
   # variable value
-  VERSION <- "1.2.0"
+  VERSION <- "1.2.0" #nolint
   si_str <- as.character(version())
   expect_true(any(grepl("<div id=\"shinyinfo_version\" class=\"info_box\">1.2.0</div>", si_str, fixed = TRUE)))
   expect_true(any(grepl("1.2.0", si_str, fixed = TRUE)))
